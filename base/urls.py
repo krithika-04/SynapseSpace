4be93f5f076fedbdde8d620305d919ac02d20900
krithika-04 <69355641+krithika-04@users.noreply.github.com>
2024-08-29 17:09:10 +0530

    add dev branch

---
+++ base/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+
+from . import views
+urlpatterns = [
+    path('login/',views.loginPage,name='login'),
+    path('register/',views.registerPage,name='register'),
+    path('logout/',views.logoutPage,name='logout'),
+    path('',views.home,name='home'),
+    path('room/<str:id>/',views.room,name='room'),
+    path('profile/<str:id>/',views.userProfile,name='user-profile'),
+    path('create-room/',views.createRoom,name='create-room'),
+    path('update-room/<str:id>/',views.updateRoom,name='update-room'),
+    path('delete-room/<str:id>/',views.deleteRoom,name='delete-room'),
+    path('delete-message/<str:id>/',views.deleteMessage,name='delete-message'),
+    path('update-user/',views.updateUser,name='update-user'),
+    path('topics/',views.topicsPage,name='topics'),
+    path('activity/',views.activityPage,name='activity'),
+]
